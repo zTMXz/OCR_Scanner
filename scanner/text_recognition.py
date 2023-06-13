6edf17839e5078bcc1ecdabdf8574d0b8e5aa1c1
zTMXz <erekhovets200226@gmail.com>
2023-06-13 19:40:13 +0300

    changed path

--- scanner/text_recognition.py
+++ scanner/text_recognition.py
@@ -3,6 +3,5 @@ import os
 
 def scan_image(img_path: str):
     reader = easyocr.Reader(['en'])
-    print('---', os.path.abspath(os.curdir))
     return reader.readtext(os.path.abspath(os.curdir) + '\\' + img_path,
                              detail=0, paragraph=True)[0]
