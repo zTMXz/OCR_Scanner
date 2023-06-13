045013adb82c6e644159b8f8472e7334304872f1
zTMXz <erekhovets200226@gmail.com>
2023-06-13 20:45:34 +0300

    removed useless model

--- scanner/models.py
+++ scanner/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from users.models import User
 
 # Create your models here.
@@ -6,11 +7,8 @@ class Scanner(models.Model):
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
-class ScanHistory(models.Model):
-    description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
     recognition = models.TextField(blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def get_absolute_url(self):
+        return reverse('scanner:image_upload_success', kwargs={'id': self.id})
 
