220b278f2fbdfe71cb7da503e9de02c7bb65bd17
zTMXz <erekhovets200226@gmail.com>
2023-06-13 21:39:35 +0300

    now also returns extended result

--- scanner/text_recognition.py
+++ scanner/text_recognition.py
@@ -1,7 +1,16 @@
 import easyocr
 import os
 
+
 def scan_image(img_path: str):
     reader = easyocr.Reader(['en'])
-    return reader.readtext(os.path.abspath(os.curdir) + '\\' + img_path,
-                             detail=0, paragraph=True)[0]
+    result = reader.readtext(os.path.abspath(os.curdir) + '\\' + img_path,
+                           detail=0, paragraph=True)[0]
+    result_extended = reader.readtext(os.path.abspath(os.curdir) + '\\' + img_path)
+
+    print(result_extended)
+    result_extended = '\n'.join([''.join(t + ' -> ' + f'{s}') for b, t, s in result_extended])
+
+    print(result_extended)
+    return result, result_extended
+
