45b25e27ec4b4fb6dba968593a84a48f82a0c892
zTMXz <erekhovets200226@gmail.com>
2023-06-22 18:53:39 +0300

    add slugify

--- scanner/models.py
+++ scanner/models.py
@@ -2,10 +2,18 @@ from django.db import models
 from django.urls import reverse
 from users.models import User
 
+from pytils import translit
+from django.utils.text import slugify
+
 # Create your models here.
 class Scanner(models.Model):
+    def get_image_path(self, filename):
+        path = ''.join(["users/scans/", translit.slugify(filename)])
+        return path
+
+
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+    image = models.ImageField(verbose_name='Изображение', upload_to=get_image_path)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     recognition = models.TextField(blank=True)
     recognition_extended = models.TextField(blank=True)
@@ -15,4 +23,5 @@ class Scanner(models.Model):
 
     def get_absolute_url(self):
         return reverse('scanner:image_upload_success', kwargs={'id': self.id})
+
 
