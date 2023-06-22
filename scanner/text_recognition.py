85cbca83719b273bafcc0bc5c56f228d1dbb2331
zTMXz <erekhovets200226@gmail.com>
2023-06-22 20:16:53 +0300

    fixed bug

--- scanner/text_recognition.py
+++ scanner/text_recognition.py
@@ -5,7 +5,8 @@ import os
 def scan_image(img_path: str):
     reader = easyocr.Reader(['en'])
     result = reader.readtext(os.path.abspath(os.curdir) + img_path,
-                           detail=0, paragraph=True)[0]
+                           detail=0, paragraph=True)
+    result = ' '.join(i for i in result)
     result_extended = reader.readtext(os.path.abspath(os.curdir) + img_path)
     result_extended = '\n'.join([''.join(text + ' -> ' + str(chance)) for borders, text, chance in result_extended])
 
