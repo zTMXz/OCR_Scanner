bfc2325f3c228fa3e090ed7ede317374f3a7e39a
zTMXz <erekhovets200226@gmail.com>
2023-06-23 11:00:24 +0300

    removed recognition_extended variable

--- scanner/text_recognition.py
+++ scanner/text_recognition.py
@@ -2,13 +2,14 @@ import easyocr
 import os
 
 
-def scan_image(img_path: str):
-    reader = easyocr.Reader(['en'])
+def scan_image(img_path: str, lang: str):
+    reader = easyocr.Reader([ lang ])
     result = reader.readtext(os.path.abspath(os.curdir) + img_path,
                            detail=0, paragraph=True)
     result = ' '.join(i for i in result)
-    result_extended = reader.readtext(os.path.abspath(os.curdir) + img_path)
-    result_extended = '\n'.join([''.join(text + ' -> ' + str(chance)) for borders, text, chance in result_extended])
 
-    return result, result_extended
+    # result_extended = reader.readtext(os.path.abspath(os.curdir) + img_path)
+    # result_extended = '\n'.join([''.join(text + ' -> ' + str(chance)) for borders, text, chance in result_extended])
+
+    return result
 
