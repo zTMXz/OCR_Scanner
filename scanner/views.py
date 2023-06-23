9cab3fc5baa3f7210cc5ae0543f3516fadafd48e
zTMXz <erekhovets200226@gmail.com>
2023-06-23 11:01:14 +0300

    upd.

--- scanner/views.py
+++ scanner/views.py
@@ -12,11 +12,12 @@ class ImageUpload(CreateView):
 
     def form_valid(self, form):
         img_obj = form.instance.image
+        lang = 'en'
 
         form.instance.user = self.request.user
         form.save()
 
-        form.instance.recognition, form.instance.recognition_extended = scan_image(img_obj.url)
+        form.instance.recognition = scan_image(img_obj.url, lang)
         form.save()
 
         return super().form_valid(form)
@@ -31,10 +32,10 @@ class ImageUploadDetail(DetailView):
         id_ = self.kwargs.get('id')
         return get_object_or_404(Scanner, id=id_)
 
-    def get_context_data(self, **kwargs):
-        context = super(ImageUploadDetail, self).get_context_data(**kwargs)
-        phrase = self.object.recognition_extended
-        ph = [line.split('->')[0].strip() for line in phrase.split('\n')]
-        chance = [line.split('->')[1].strip() for line in phrase.split('\n')]
-        context['phrase_chance'] = zip(ph, chance)
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super(ImageUploadDetail, self).get_context_data(**kwargs)
+    #     phrase = self.object.recognition_extended
+    #     ph = [line.split('->')[0].strip() for line in phrase.split('\n')]
+    #     chance = [line.split('->')[1].strip() for line in phrase.split('\n')]
+    #     context['phrase_chance'] = zip(ph, chance)
+    #     return context
