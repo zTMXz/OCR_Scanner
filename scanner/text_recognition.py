d47a2ebc43b50b553d38eeb82cd452c9a94a4869
zTMXz <erekhovets200226@gmail.com>
2023-06-28 14:59:28 +0300

    upd to support GPU

--- scanner/text_recognition.py
+++ scanner/text_recognition.py
@@ -3,7 +3,7 @@ import os
 
 
 def scan_image(img_path: str, lang: str):
-    reader = easyocr.Reader([ lang ])
+    reader = easyocr.Reader([lang])
     result = reader.readtext(os.path.abspath(os.curdir) + img_path,
                            detail=0, paragraph=True)
     result = ' '.join(i for i in result)
