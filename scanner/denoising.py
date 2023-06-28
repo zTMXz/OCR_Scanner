d7eee708b5a02add310454c97b327c4b7f0c3e0b
zTMXz <erekhovets200226@gmail.com>
2023-06-28 23:41:47 +0300

    upd

--- scanner/denoising.py
+++ scanner/denoising.py
@@ -6,7 +6,6 @@ import tensorflow as tf
 
 from PIL import Image
 
-
 def process_image(path):
     img = cv2.imread(path)
     img = np.asarray(img, dtype = "float32")
@@ -16,7 +15,6 @@ def process_image(path):
     img = np.reshape(img, (420, 540, 1))
 
     return img
-
 
 def denoise_image(path: str):
     path_to_model = os.path.abspath(os.curdir) + '\model_learning\model_full.h5'
@@ -32,11 +30,12 @@ def denoise_image(path: str):
     plt.plot(1, 2, 2)
     plt.xticks([])
     plt.yticks([])
+    plt.axis('off')
     plt.imshow(result[0][:, :, 0], cmap='gray')
 
     path_img = path_img + '_denoised.png'
 
-    plt.savefig(path_img, transparent=True)
+    plt.savefig(path_img, bbox_inches='tight', transparent=True)
     Image.open(path_img).resize(size).save(path_img)
 
     return path+'_denoised.png'
